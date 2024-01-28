3a22722e39cfb0a99c31f2d7def622f281ee17c7
Karollo999 <157982851+Karollo999@users.noreply.github.com>
2024-01-28 19:13:08 +0100

    Update LibraryTest.java

--- Library-Zaliczenie/test/LibraryTest.java
+++ Library-Zaliczenie/test/LibraryTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;*
+import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     private Library library;
